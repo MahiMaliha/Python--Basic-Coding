6f8b61dacebeed4330632e9e6ea5fbc9eb0a0198
Jannatul Mahiya Mutasha <87617184+JannatulMutashaEngineer@users.noreply.github.com>
2021-09-11 17:57:54 +0600

    Add files via upload

---
+++ forloop.py
@@ -0,0 +1,35 @@
+for name in ["shakib","tamim","mashrafee","mushfiqur","rubel","mahmudullah"]:
+    print("hi",name,"you are play in the next match") 
+
+# Now we will see
+
+for villiers in "best batting performance":
+    print(villiers)    
+
+#how many times is the word LM10    
+M="great messi"
+for L in M:
+    print("LM10")
+
+#turtle
+import turtle
+git=turtle.Screen()
+git.bgcolor('lightgreen')     
+github=turtle.Turtle()
+github.pensize(5)
+
+for gColor in ["yellow","red","purple","blue"]:    
+    github.color(gColor)
+    github.forward(180)
+    github.right(90)
+git.exitonclick()
+#naming 
+
+v=range(9)
+tot=7
+for num in v:
+    print(num)
+    tot /= 9
+print(tot) 
+    
+
